86e286eaa3134411622b716b1f80a106d2682f83
efabriko <efabriko@gmail.com>
2018-03-07 22:53:22 +0900

    4.5 Views, Partials etc...

--- app/controllers/simple_pages_controller.rb
+++ app/controllers/simple_pages_controller.rb
@@ -1,7 +1,8 @@
 class SimplePagesController < ApplicationController
   
 	def landing_page
-    @featured_product = Product.first
+    @featured_product = Product.all
+    @products = Product.limit(3)
   end
 
   def index
