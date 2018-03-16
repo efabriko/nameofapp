9224aa879e719eec4de6ced3b6b0169ef904399c
efabriko <efabriko@gmail.com>
2018-03-16 23:42:50 +0900

    Update Carousel

--- app/controllers/simple_pages_controller.rb
+++ app/controllers/simple_pages_controller.rb
@@ -2,7 +2,7 @@ class SimplePagesController < ApplicationController
   
 	def landing_page
     @featured_product = Product.all
-    @products = Product.last(3)
+    @products = Product.limit(3)
   end
 
   def index
