3f3a33e4ef5e488cb8b8c0ad8f00f228d5714f75
efabriko <efabriko@gmail.com>
2018-04-02 21:41:05 +0900

    5.2 forms

--- config/routes.rb
+++ config/routes.rb
@@ -10,8 +10,8 @@ Rails.application.routes.draw do
 
   get 'simple_pages/landing_page'
 
-  root 'products#index'
-  # root 'simple_pages#landing_page'
+  # root 'products#index'
+  root 'simple_pages#landing_page'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
 	resources :orders, only: [:index, :show, :create, :destroy]
